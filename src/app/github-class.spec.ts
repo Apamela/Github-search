e384673dab56098cbdc1ab47342e1eab09dc84cb
pamela sindy <pamelab.desire@gmail.com>
2019-08-29 14:28:40 +0200

    correct the error

--- src/app/github-class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GithubClass } from './github-class';
-
-describe('GithubClass', () => {
-  it('should create an instance', () => {
-    expect(new GithubClass()).toBeTruthy();
-  });
-});
--- src/app/github-class.ts
+++
@@ -1,3 +0,0 @@
-export class GithubClass {
-    constructor(public author:string, public github:string){}
-}
