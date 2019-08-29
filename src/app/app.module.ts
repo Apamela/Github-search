ff4508e799828cf9cb4f2b6d5bd14c7082e11cde
pamela sindy <pamelab.desire@gmail.com>
2019-08-29 17:05:27 +0200

    add the codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormSearchComponent } from './form-search/form-search.component';
+import { DateCountPipe } from './date-count.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FormSearchComponent,
+    DateCountPipe,
     
   ],
   imports: [
--- src/app/form-search/form-search.component.ts
+++ src/app/form-search/form-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-form-search',
   templateUrl: './form-search.component.html',
